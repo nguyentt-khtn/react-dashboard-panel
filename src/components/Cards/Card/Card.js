825131ffbbb9b070e425bf3ee5c85d1abf318f60
nguyentt <nguyentt@360.org.vn>
2022-06-15 16:39:29 +0700

    complete expand card and code table

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import { CircularProgressbar } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 import './Card.css'
-import { AnimateSharedLayout } from "framer-motion"
+import { AnimateSharedLayout, motion } from "framer-motion"
+import Chart from 'react-apexcharts'
 
 export default function Card(props) {
     const [state, setState] = useState(true)
@@ -18,9 +19,9 @@ export default function Card(props) {
 
 function CompactCard(props) {
     let { barValue, color, icon, series, title, value } = props.atts
-    let {setState} = props
+    let { setState } = props
     return (
-        <div className='card' style={{ background: color.backGround, boxShadow: color.boxShadow }} onClick={()=>{setState(false)}}>
+        <motion.div layoutId='expandedCard' className='card' style={{ background: color.backGround, boxShadow: color.boxShadow }} onClick={() => { setState(false) }}>
             <div className='col-1'>
                 <div className='circle'><CircularProgressbar value={barValue} text={`${barValue}%`} /></div>
                 <div className='title'>{title}</div>
@@ -30,18 +31,70 @@ function CompactCard(props) {
                 <div>${value}</div>
                 <div>Last 24 hours</div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
 function ExpandedCard(props) {
     let { barValue, color, icon, series, title, value } = props.atts
+    let options = {
+        chart: {
+            type: 'area',
+            height: 'auto'
+        },
+        dropShadow: {
+            enable: false,
+            enableOnSeries: undefined,
+            top: 0,
+            left: 0,
+            blur: 3,
+            color: '#000',
+            opacity: 0.35
+        },
+        fill: {
+            colors: ['#fff'],
+            type: 'gradient',
+        },
+        dataLabels: {
+            enable: false,
+        },
+        stroke: {
+            curve: 'smooth',
+            colors: ['white']
+        },
+        tootip: {
+            x: {
+                format: 'dd/MM/yy HH:mm'
+            }
+        },
+        grid: {
+            show: true
+        },
+        xaxis: {
+            type: 'datatime',
+            categories: [
+                '2018-09-19T00:00:00.000Z',
+                '2018-09-19T01:30:00.000Z',
+                '2018-09-19T02:30:00.000Z',
+                '2018-09-19T03:30:00.000Z',
+                '2018-09-19T04:30:00.000Z',
+                '2018-09-19T05:30:00.000Z',
+                '2018-09-19T06:30:00.000Z',
+            ]
+        },
+
+
+    }
     let { setState } = props
     return (
-        <div className='expanded-card' style={{ background: color.backGround, boxShadow: color.boxShadow }}>
-            <i className="fas fa-times" onClick={()=>{setState(true)}}></i>
-            <div>{title}</div>
-            <div>Last 24 hour</div>
-        </div>
+        <motion.div className='expanded-card' style={{ background: color.backGround, boxShadow: color.boxShadow }}
+        layoutId='expandedCard'>
+            <i style={{alignSelf:'flex-end',marginTop:'-15px', color:'#fff', marginBottom:'5px'}} className="fas fa-times" onClick={() => { setState(true) }}></i>
+            <div style={{fontSize:'35px', color:'white'}}>{title}</div>
+            <div className='chart'>
+                <Chart options={options} series={series} type="area"/>
+            </div>
+            <div style={{color:'white', fontWeight:'700'}}>Last 24 hour</div>
+        </motion.div>
     )
 }
--- src/components/MainDash/MainDash.js
+++ src/components/MainDash/MainDash.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Cards from '../Cards/Cards'
+import Table from '../Table/TableOrder'
 import './MainDash.css'
 
 export default function MainDash() {
     return (
         <div className='main-dash'>
-            <h3>asodaksd</h3>
+            <h3>Dashboard</h3>
             <Cards />
+            <h3>Recent Orders</h3>
+            <Table />
         </div>
     )
 }
